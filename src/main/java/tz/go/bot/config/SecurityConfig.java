052a39f0862e7d44d50b6590b2e9a5bbc8b4bb84
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-10 15:19:04 +0530

    Added Basic Configuration

---
+++ src/main/java/tz/go/bot/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package tz.go.bot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws  Exception{
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+}
--- src/main/java/tz/go/bot/controller/AccountController.java
+++ src/main/java/tz/go/bot/controller/AccountController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController {
 
-    @GetMapping("accounts")
+    @GetMapping("/accounts")
     public String getAccountDetails(String input) {
         return "Here are the account details from the DB";
     }
