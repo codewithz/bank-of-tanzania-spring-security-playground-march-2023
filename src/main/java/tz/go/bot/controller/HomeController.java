faf1655ecac9b2ec359d8279cbb85010cd962044
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-10 14:59:45 +0530

    Initial commit

---
+++ src/main/java/tz/go/bot/controller/HomeController.java
@@ -0,0 +1,14 @@
+package tz.go.bot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Spring Security";
+    }
+
+}
