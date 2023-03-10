bd0469f435514a38f66e171c87d1bda718ac35ef
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-10 19:06:26 +0530

    Updated Script for BCrypt

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -43,3 +43,28 @@ VALUES (1, 'tom', '12345', '1');
 INSERT INTO public.authorities(
     id, username, authority)
 VALUES (1, 'tom', 'write');
+
+-- Table: public.bank_customer
+
+-- DROP TABLE IF EXISTS public.bank_customer;
+
+CREATE TABLE IF NOT EXISTS public.bank_customer
+(
+    id integer NOT NULL DEFAULT 'nextval('bank_customer_id_seq'::regclass)',
+    email text COLLATE pg_catalog."default" NOT NULL,
+    password text COLLATE pg_catalog."default" NOT NULL,
+    role text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT bank_customer_pkey PRIMARY KEY (id)
+    )
+
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.bank_customer
+    OWNER to postgres;
+
+
+INSERT INTO public.bank_customer(
+    id, email, password, role)
+VALUES (1, 'john.doe@example.com', '$2a$12$aD7LV2l7uVGaJF.Vgs2qJOapihYlja4X6MCCAiRheerF7iNKOK07i', 'admin');
+
+https://bcrypt-generator.com/
