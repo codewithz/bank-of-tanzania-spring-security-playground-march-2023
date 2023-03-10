b535a32599ea7cc2af5a07a36cfb9b3d1d2c163e
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-10 16:50:00 +0530

    PermitAll and Authenticated

--- src/main/java/tz/go/bot/config/SecurityConfig.java
+++ src/main/java/tz/go/bot/config/SecurityConfig.java
@@ -7,15 +7,36 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
+//    /accounts --> Secured
+//    /balance --> Secured
+//    /loans --> Secured
+//    /cards --> Secured
+//    /notices --> Not Secured
+//    /contact --> Not Secured
+
     @Override
     public void configure(HttpSecurity http) throws  Exception{
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .and()
+//                .httpBasic();
+
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/accounts").authenticated()
+                .antMatchers("/balance").authenticated()
+                .antMatchers("/cards").authenticated()
+                .antMatchers("/loans").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
                 .and()
                 .formLogin()
                 .and()
                 .httpBasic();
+
     }
 
 }
